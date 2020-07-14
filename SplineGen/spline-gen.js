e33159e31d95c8236b1fc53f81f0a8ae66e940da
Remi Pierot <rfpierot91@gmail.com>
2020-07-14 23:18:52 +0900

    SplineGen overhaul to use a class-based architecture

--- SplineGen/spline-gen.js
+++ SplineGen/spline-gen.js
@@ -1,334 +1,264 @@
-import { 
-	fillWindowWith, 
-	remap, 
-	plotCurveToSVG,
-	SVGPathToCanvas
-} from '../dependencies/utils.js';
+import   Utils         from '../dependencies/utils.js';
+import { Bounds      } from '../dependencies/maths.js';
+import { VarSlider   } from '../dependencies/components.js';
+import { VarColor    } from '../dependencies/components.js';
+import { VarCheckbox } from '../dependencies/components.js';
 
-var Param = {
-	A: 0,
-	B: 1,
-	C: 2,
-	D: 3,
-	J: 4,
-	K: 5,
-	Precision: 6,
-	Thickness: 7,
-	OTThickness: 8
+class Engine {
+	#plotPanel
+	#svgPlot
+	#data
+
+	get a() { return this.#data.a; }
+	get b() { return this.#data.b; }
+	get c() { return this.#data.c; }
+	get d() { return this.#data.d; }
+	get j() { return this.#data.j; }
+	get k() { return this.#data.k; }
+	get precision()              { return this.#data.precision;              }
+	get curveThickness()         { return this.#data.curveThickness;         }
+	get curveColor()             { return this.#data.curveColor;             }
+	get outlineThickness()       { return this.#data.outlineThickness;       }
+	get outlineColor()           { return this.#data.outlineColor;           }
+	get backgroundTransparency() { return this.#data.backgroundTransparency; }
+	get backgroundColor()        { return this.#data.backgroundColor;        }
+	get maxThickness()           { return this.#data.maxThickness;           }
+
+	get fileName() { 
+		return "SplineGen(a" + this.a.value + 
+				"-b" + this.b.value +
+				"-c" + this.c.value +
+				"-d" + this.d.value +
+				"-j" + this.j.value +
+				"-k" + this.k.value + ")"; 
+	}
+
+	constructor(plotID, svgPlotID, maxThickness) {
+		this.#plotPanel = document.getElementById(plotID);
+		this.#svgPlot = document.getElementById(svgPlotID);
+		
+		this.#data = {
+			a: { value: 0 },
+			b: { value: 0 },
+			c: { value: 0 },
+			d: { value: 0 },
+			j: { value: 1 },
+			k: { value: 1 },
+			precision:              { value: 500          },
+			curveThickness:         { value: 20           },
+			curveColor:             { value: '\#000000'   },
+			outlineThickness:       { value: 20           },
+			outlineColor:           { value: '\#FFFFFF'   },
+			backgroundTransparency: { value: false        },
+			backgroundColor:        { value: '\#000000'   },
+			maxThickness:           { value: maxThickness }
+		}
+	}
+
+	drawCurve() {
+		let fullThickness = this.curveThickness.value + this.outlineThickness.value;
+		let adjustedThickness = fullThickness + (fullThickness / this.maxThickness.value) * 100;
+		let demiThickness = .5 * adjustedThickness;
+		let w = Number(this.#svgPlot.width.baseVal.value);
+		let h = Number(this.#svgPlot.height.baseVal.value);
+		let drawBackground = !this.backgroundTransparency.value;
+	
+		//Fetch the curve (each point having normalized coordinates)
+		let curve = this.getCurve(
+			this.a.value,
+			this.b.value,
+			this.c.value,
+			this.d.value,
+			this.j.value,
+			this.k.value,
+			this.precision.value,
+			true
+		);
+	
+		//Remap the curve points to the svg space
+		let newPoints = [];
+		for(let i = 0; i < curve.length; i++) {
+			newPoints.push({
+				x: demiThickness + curve[i].x * (w - adjustedThickness), 
+				y: demiThickness + curve[i].y * (h - adjustedThickness)
+			});
+		}
+	
+		if (drawBackground) {
+			this.#plotPanel.style.backgroundColor = this.backgroundColor.value;
+			this.#plotPanel.style.backgroundImage = "none";
+		}
+		else {
+			this.#plotPanel.style.backgroundColor = "transparent";
+			this.#plotPanel.style.backgroundImage = "url('checkerboard.png')";
+		}
+	
+		let svgClone = this.#svgPlot.cloneNode(false);
+		this.#plotPanel.removeChild(this.#svgPlot);
+		this.#plotPanel.appendChild(svgClone);
+		this.#svgPlot = svgClone;
+		
+		if(newPoints.length > 1) {
+			//Draw outline first
+			if(this.outlineThickness.value > 0) {
+				Utils.plotCurveToSVG(
+					newPoints, 
+					fullThickness, 
+					this.outlineColor.value, 
+					this.#svgPlot, 
+					drawBackground ? this.backgroundColor.value : "none"
+				);
+				drawBackground = false;
+			}
+	
+			Utils.plotCurveToSVG(
+				newPoints, 
+				this.curveThickness.value, 
+				this.curveColor.value, 
+				this.#svgPlot, 
+				drawBackground ? this.backgroundColor.value : "none"
+			);
+		}
+	}
+
+	getCurve(a, b, c , d , j, k, nbPoints, centered = false) {
+		let curve = [];
+		let min = { x: Number.MAX_VALUE, y: Number.MAX_VALUE };
+		let max = { x: Number.MIN_VALUE, y: Number.MIN_VALUE };
+	
+		for(let t = 0; t <= nbPoints; t++) {
+			let point = this.getCurvePoint(a, b, c , d , j, k, t / nbPoints);
+			point.x   = Bounds.remapUsingValues(point.x, -2, 2, 0, 1);
+			point.y   = Bounds.remapUsingValues(point.y, -2, 2, 0, 1);
+			curve.push(point);
+	
+			if(min.x > point.x) { min.x = point.x; }
+			if(min.y > point.y) { min.y = point.y; }
+			if(max.x < point.x) { max.x = point.x; }
+			if(max.y < point.y) { max.y = point.y; }
+		}
+	
+		if(centered) {
+			let center   = { x: (min.x + max.x) / 2, y: (min.y + max.y) / 2 };
+			let toCenter = { x: .5 - center.x,       y: .5 - center.y       };
+	
+			curve.forEach(function(point){
+				point.x += toCenter.x;
+				point.y += toCenter.y;
+			});
+		}
+	
+		return curve;
+	}
+	
+	getCurvePoint(a, b, c, d, j, k, t) {
+		let point = { x: 0, y: 0 };
+		let angle = t * 2 * Math.PI;
+	
+		//j is linked to cos(b * angle), so if b is 0, j will not have any effect on the curve
+		//j is a power value, so if it is 0, b will not have any effect on the curve
+		point.x = Math.cos(a * angle) - Math.pow(Math.cos(b * angle), j);
+	
+		//k is linked to sin(d * angle), so if d is 0, k will not have any effect on the curve
+		//k is a power value, so if it is 0, d will not have any effect on the curve
+		point.y = Math.sin(c * angle) - Math.pow(Math.sin(d * angle), k);
+	
+		return point;
+	}
+
+	updatePlotDimensions() {
+		let target = Math.min(this.#plotPanel.offsetWidth, this.#plotPanel.offsetHeight);
+		this.#svgPlot.setAttribute("width",  target + "px");
+		this.#svgPlot.setAttribute("height", target + "px");
+	}
+
+	savePNG(button) {
+		let canvasPlot = document.createElement("canvas");
+		Utils.SVGPathToCanvas(this.#svgPlot, canvasPlot);
+		button.href = canvasPlot.toDataURL("image/png");
+		button.download = `${this.fileName}.png`;
+	}
+
+	saveSVG(button) {
+		let svgBlob = new Blob([this.#svgPlot.outerHTML], {type:"image/svg+xml;charset=utf-8"});
+		button.href = URL.createObjectURL(svgBlob);
+		button.download = `${this.fileName}.svg`;
+	}
 }
 
-var app = document.getElementById("app");
-var settingsPanel = document.getElementById("settings-panel");
-var settings = document.getElementById("settings");
-var collapseSettings = document.getElementById("collapse-settings");
-var plotPanel = document.getElementById("plot-panel");
-var svgPlot = document.getElementById("svg-plot");
-var canvasPlot = document.createElement("canvas");
+const maxCurveThickness = 30;
+const maxOutlineThickness = 30;
 
-var curveColor = document.getElementById("curve-color");
-var outlineColorLabel = document.getElementById("ot-color-label");
-var outlineColor = document.getElementById("ot-color");
-var bckgTransparency = document.getElementById("background-transparency");
-var bckgColorLabel = document.getElementById("background-color-label");
-var bckgColor = document.getElementById("background-color");
-var collapseLabel = document.getElementById("collapse-label");
+const engine = new Engine("plot-panel", "svg-plot", maxCurveThickness + maxOutlineThickness);
 
-var sliders = [
-				document.getElementById("a-slider"),
-				document.getElementById("b-slider"),
-				document.getElementById("c-slider"),
-				document.getElementById("d-slider"),
-				document.getElementById("j-slider"),
-				document.getElementById("k-slider"),
-				document.getElementById("precision-slider"),
-				document.getElementById("thickness-slider"),
-				document.getElementById("ot-thickness-slider")
-			];
+const varSliders = {
+	a:                new VarSlider("a-slider",  "a-range-label",  engine.a,                0, 10,                  2, 0, true, () => engine.drawCurve()),
+	b:                new VarSlider("b-slider",  "b-range-label",  engine.b,                0, 10,                  2, 0, true, () => engine.drawCurve()),
+	c:                new VarSlider("c-slider",  "c-range-label",  engine.c,                0, 10,                  2, 0, true, () => engine.drawCurve()),
+	d:                new VarSlider("d-slider",  "d-range-label",  engine.d,                0, 10,                  2, 0, true, () => engine.drawCurve()),
+	j:                new VarSlider("j-slider",  "j-range-label",  engine.j,                1, 3,                   2, 0, true, () => engine.drawCurve()),
+	k:                new VarSlider("k-slider",  "k-range-label",  engine.k,                1, 3,                   2, 0, true, () => engine.drawCurve()),
+	precision:        new VarSlider("p-slider",  "p-range-label",  engine.precision,        1, 500,                 3, 0, true, () => engine.drawCurve()),
+	curveThickness:   new VarSlider("ct-slider", "ct-range-label", engine.curveThickness,   1, maxCurveThickness,   3, 0, true, () => engine.drawCurve()),
+	outlineThickness: new VarSlider("ot-slider", "ot-range-label", engine.outlineThickness, 0, maxOutlineThickness, 3, 0, true, () => {
+		varColors.outlineColor.enabled = varSliders.outlineThickness.targetValue > 0;
+		engine.drawCurve();
+	}),
+};
 
-var labels = [
-				document.getElementById("a-range-label"),
-				document.getElementById("b-range-label"),
-				document.getElementById("c-range-label"),
-				document.getElementById("d-range-label"),
-				document.getElementById("j-range-label"),
-				document.getElementById("k-range-label"),
-				document.getElementById("precision-range-label"),
-				document.getElementById("thickness-range-label"),
-				document.getElementById("ot-thickness-range-label")
-			];
+const varColors = {
+	curveColor:      new VarColor("curve-color",      "curve-color-label",      engine.curveColor,      () => engine.drawCurve()),
+	outlineColor:    new VarColor("outline-color",    "outline-color-label",    engine.outlineColor,    () => engine.drawCurve()),
+	backgroundColor: new VarColor("background-color", "background-color-label", engine.backgroundColor, () => engine.drawCurve())
+}
 
-//Setup listeners
+const varCheckboxes = {
+	backgroundTransparency: new VarCheckbox("background-transparency", "", engine.backgroundTransparency, () => {
+		varColors.backgroundColor.enabled = !varCheckboxes.backgroundTransparency.targetValue;
+		engine.drawCurve();
+	})
+}
+
 window.addEventListener("load", function(){
-	updateCollapseLabel();
-	updatePlotDimensions()
-	drawCurve();
-	updateLabels();
+	let p = Utils.getURLParameters();
+
+	varSliders.a.modifyTargetValue(p.a);
+	varSliders.b.modifyTargetValue(p.b);
+	varSliders.c.modifyTargetValue(p.c);
+	varSliders.d.modifyTargetValue(p.d);
+	varSliders.j.modifyTargetValue(p.j);
+	varSliders.k.modifyTargetValue(p.k);
+	varSliders.precision.modifyTargetValue(p.precision);
+	varSliders.curveThickness.modifyTargetValue(p.curveThickness);
+	varSliders.outlineThickness.modifyTargetValue(p.outlineThickness);
+	varColors.curveColor.modifyTargetValue(`#${p.curveColor}`);
+	varColors.outlineColor.modifyTargetValue(`#${p.outlineColor}`);
+	varColors.backgroundColor.modifyTargetValue(`#${p.backgroundColor}`);
+	varCheckboxes.backgroundTransparency.modifyTargetValue(p.backgroundTransparency === 'true');
+
+	engine.updatePlotDimensions();
+	engine.drawCurve();
 });
 
 window.addEventListener("resize", function(){
-	updatePlotDimensions();
-	drawCurve();
-	updateLabels();
+	engine.updatePlotDimensions();
+	engine.drawCurve();
 });
-
-for(let i = Param.A; i <= Param.OTThickness; i++) {
-	sliders[i].addEventListener("input", function(){
-		drawCurve();
-		updateLabels();
-	});
-}
 
 document.getElementById("randomize").addEventListener("click", function(){
-	randomizeValues()
-	updatePlotDimensions()
-	drawCurve();
-	updateLabels();
+	varSliders.a.randomizeElementValue();
+	varSliders.b.randomizeElementValue();
+	varSliders.c.randomizeElementValue();
+	varSliders.d.randomizeElementValue();
+	varSliders.j.randomizeElementValue();
+	varSliders.k.randomizeElementValue();
+	engine.drawCurve();
 });
-
-curveColor.addEventListener("input", function(){
-	drawCurve();
-})
-
-sliders[Param.OTThickness].addEventListener("input", function(){
-	if(this.value == 0) {
-		outlineColorLabel.setAttribute("disabled", "disabled");
-		outlineColor.setAttribute("disabled", "disabled");
-	}
-	else {
-		outlineColorLabel.removeAttribute("disabled");
-		outlineColor.removeAttribute("disabled");
-	}
-
-	drawCurve();
-});
-
-outlineColor.addEventListener("input", function(){
-	drawCurve();
-})
-
-bckgTransparency.addEventListener("input", function(){
-	if(this.checked) {
-		bckgColorLabel.setAttribute("disabled", "disabled");
-		bckgColor.setAttribute("disabled", "disabled");
-		plotPanel.style.backgroundColor = "transparent";
-		plotPanel.style.backgroundImage = "url('checkerboard.png')";
-	}
-	else {
-		bckgColorLabel.removeAttribute("disabled");
-		bckgColor.removeAttribute("disabled");
-		plotPanel.style.backgroundColor = bckgColor.value;
-		plotPanel.style.backgroundImage = "none";
-	}
-
-	drawCurve();
-})
-
-bckgColor.addEventListener("input", function(){
-	drawCurve();
-})
 
 document.getElementById("save-png").addEventListener("click", function(){
-    SVGPathToCanvas(svgPlot, canvasPlot);
-
-    this.href = canvasPlot.toDataURL("image/png");
-    this.download="SplineGen(a" + sliders[Param.A].value + 
-    				"-b" + sliders[Param.B].value +
-    				"-c" + sliders[Param.C].value +
-    				"-d" + sliders[Param.D].value +
-    				"-j" + sliders[Param.J].value +
-    				"-k" + sliders[Param.K].value + ").png"
+    engine.savePNG(this);
 });
 
 document.getElementById("save-svg").addEventListener("click", function(){
-	let svgBlob = new Blob([svgPlot.outerHTML], {type:"image/svg+xml;charset=utf-8"});
-
-    this.href = URL.createObjectURL(svgBlob);
-    this.download="SplineGen(a" + sliders[Param.A].value + 
-    				"-b" + sliders[Param.B].value +
-    				"-c" + sliders[Param.C].value +
-    				"-d" + sliders[Param.D].value +
-    				"-j" + sliders[Param.J].value +
-    				"-k" + sliders[Param.K].value + ").svg"
+    engine.saveSVG(this);
 });
-
-collapseSettings.addEventListener("click", function(){
-	let collapsed = settings.getAttribute("collapsed") === "collapsed";
-
-	if(collapsed) {
-		settingsPanel.setAttribute("collapsed", "expanded");
-		settings.setAttribute("collapsed", "expanded");
-		this.setAttribute("collapsed", "expanded");
-		plotPanel.setAttribute("collapsed", "expanded");
-	}
-	else {
-		settingsPanel.setAttribute("collapsed", "collapsed");
-		settings.setAttribute("collapsed", "collapsed");
-		this.setAttribute("collapsed", "collapsed");
-		plotPanel.setAttribute("collapsed", "collapsed");
-	}
-
-	updateCollapseLabel();
-
-	setTimeout( () =>{
-		updatePlotDimensions();
-		drawCurve();
-	}, 50);
-
-	setTimeout( () =>{
-		updatePlotDimensions();
-		drawCurve();
-	}, 100);
-});
-
-//Ensure the svg takes as much space as possible while keeping a square shape and without having to scroll
-function updatePlotDimensions() {
-	fillWindowWith(app);
-
-	let target = Math.min(plotPanel.offsetWidth, app.offsetHeight - 4);
-	svgPlot.setAttribute("width", target + "px");
-	svgPlot.setAttribute("height", target + "px");
-}
-
-function updateCollapseLabel() {
-	let collapsed = settings.getAttribute("collapsed") === "collapsed";
-
-	if(collapsed) {
-		collapseLabel.innerHTML = ">>";
-	}
-	else {
-		collapseLabel.innerHTML = "<<";
-	}
-}
-
-function drawCurve() {
-	let curveThickness = Number(sliders[Param.Thickness].value);
-	let outlineThickness = Number(sliders[Param.OTThickness].value);
-	let fullThickness = curveThickness + outlineThickness;
-	let MaxThickness = Number(sliders[Param.Thickness].max) + Number(sliders[Param.OTThickness].max);
-	let adjustedThickness = fullThickness + (fullThickness / MaxThickness) * 100;
-	let demiThickness = .5 * adjustedThickness;
-	let w = Number(svgPlot.width.baseVal.value);
-	let h = Number(svgPlot.height.baseVal.value);
-	let drawBackground = !bckgTransparency.checked;
-
-	//Fetch the curve (each point having normalized coordinates)
-	let curve = getCurve(
-		sliders[Param.A].value,
-		sliders[Param.B].value,
-		sliders[Param.C].value,
-		sliders[Param.D].value,
-		sliders[Param.J].value,
-		sliders[Param.K].value,
-		sliders[Param.Precision].value,
-		true,
-		true);
-
-	//Remap the curve points to the svg space
-	let newPoints = [];
-	for(let i = 0; i < curve.length; i++) {
-		newPoints.push([
-			demiThickness + curve[i][0] * (w - adjustedThickness), 
-			demiThickness + curve[i][1] * (h - adjustedThickness)
-		]);
-	}
-
-	plotPanel.style.backgroundColor = bckgColor.value;
-
-	let svgClone = svgPlot.cloneNode(false);
-	plotPanel.removeChild(svgPlot);
-	plotPanel.appendChild(svgClone);
-	svgPlot = svgClone;
-	
-	if(newPoints.length > 1) {
-		//Draw outline first
-		if(outlineThickness > 0) {
-			plotCurveToSVG(newPoints, fullThickness, outlineColor.value, svgPlot, drawBackground ? bckgColor.value : "none");
-			drawBackground = false;
-		}
-
-		plotCurveToSVG(newPoints, curveThickness, curveColor.value, svgPlot, drawBackground ? bckgColor.value : "none");
-	}
-}
-
-function getCurve(a, b, c , d , j, k, nbPoints, normalized = false, centered = false) {
-	let curve = [];
-	let min = [Number.MAX_VALUE, Number.MAX_VALUE];
-	let max = [Number.MIN_VALUE, Number.MIN_VALUE];
-
-	for(let t = 0; t <= nbPoints; t++) {
-		let point = getCurvePoint(a, b, c , d , j, k, t / nbPoints);
-		point[0] = remap(point[0], -2, 2, 0, 1, !normalized);
-		point[1] = remap(point[1], -2, 2, 0, 1, !normalized);
-		curve.push(point);
-
-		if(min[0] > point[0]) { min[0] = point[0]; }
-		if(min[1] > point[1]) { min[1] = point[1]; }
-		if(max[0] < point[0]) { max[0] = point[0]; }
-		if(max[1] < point[1]) { max[1] = point[1]; }
-	}
-
-	if(centered) {
-		let center = [(min[0] + max[0]) / 2, (min[1] + max[1]) / 2];
-		let toCenter = [.5 - center[0], .5 - center[1]];
-
-		curve.forEach(function(point){
-			point[0] += toCenter[0];
-			point[1] += toCenter[1];
-		});
-	}
-
-	return curve;
-}
-
-function getCurvePoint(a, b, c, d, j, k, t) {
-	let point = [0, 0]
-	let angle = t * 2 * Math.PI;
-
-	//j is linked to cos(b * angle), so if b is 0, j will not have any effect on the curve
-	//j is a power value, so if it is 0, b will not have any effect on the curve
-	point[0] = Math.cos(a * angle) - Math.pow(Math.cos(b * angle), j);
-
-	//k is linked to sin(d * angle), so if d is 0, k will not have any effect on the curve
-	//k is a power value, so if it is 0, d will not have any effect on the curve
-	point[1] = Math.sin(c * angle) - Math.pow(Math.sin(d * angle), k);
-
-	return point;
-}
-
-function randomizeValues() {
-	for(let i = Param.A; i <= Param.K; i++) {
-		sliders[i].value = Number(sliders[i].min) + Math.floor(Math.random() * (Number(sliders[i].max - sliders[i].min) + 1));
-	} 
-}
-
-//Change the labels content to reflect the parameter values
-function updateLabels() {
-	//Parameters
-	padLabels(Param.A, Param.K);
-
-	//Drawing options
-	padLabels(Param.Precision, Param.OTThickness);
-}
-
-function padLabels(start, end) {
-	let nbDigits = getHighestNbDigits(start, end);
-	for(let i = start; i <= end; i++) {
-		let labelContent = " (" + sliders[i].value.toString().padStart(nbDigits, '0') +
-						" / " + sliders[i].max.padStart(nbDigits, '0') + ")";
-		labels[i].innerHTML = labelContent;
-	};
-}
-
-function getHighestNbDigits(start, end) {
-	let highestNbDigits = 0;
-
-	for(let i = start; i <= end; i++) {
-		let nbDigits = Math.log10(sliders[i].max) + 1;
-
-		if(nbDigits > highestNbDigits) {
-			highestNbDigits = nbDigits;
-		}
-	}
-
-	return highestNbDigits;
-}
